7abcf17262deb892680e648693f1941520bbdbd0
david <15035990976@163.com>
2020-04-23 20:08:49 +0800

    自定义url处理类&&数据库创建

--- manager.py
+++ manager.py
@@ -1,5 +1,6 @@
 from application import app, manager
 from flask_script import Server
+import urls
 
 # web 服务器
 manager.add_command("runserver", Server(host="localhost", port=5000, use_debugger=True, use_reloader=True))
--- web/controllers/user/User.py
+++ web/controllers/user/User.py
@@ -1,10 +1,39 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, jsonify
 
 
 router_user = Blueprint("user_page", __name__)
 
-@router_user.route("/login")
+@router_user.route("/login", methods=["POST", "GET"])
 def login():
-    return "login页面"
+    if request.method == "GET":
+        return render_template('user/login.html')
+    resp = {
+        "code":200,
+        "msg":"登陆成功",
+        "data":{}
+    }
+    req = request.values
+    login_name = req["login_name"]
+    login_pwd = req["login_pwd"]
+    # 后端校检
+    if login_name is None or len(login_name) < 1:
+        resp["code"] = -1
+        resp["msg"] = "请输入正确的用户名"
+        return jsonify(resp)
+    if login_pwd is None or len(login_pwd) < 1:
+        resp["code"] = -1
+        resp["msg"] = "请输入正确的密码"
+        return jsonify(resp)
+    return jsonify(resp)
 
+# @router_user.route("/logout")
+# def logout():
+#     return "logout页面"
 
+# @router_user.route("/edit")
+# def edit():
+#     return "edit页面"
+
+# @router_user.route("/reset-pwd")
+# def resetPwd():
+#     return "重置密码 页面"
