67e1051e78249d0a13d1273dc608b2741242054d
david <15035990976@163.com>
2020-04-26 20:22:44 +0800

    '拦截器'

--- manager.py
+++ manager.py
@@ -1,14 +1,12 @@
-from application import app, manager
+from application import app,manager
 from flask_script import Server
 import urls
 
 # web 服务器
-manager.add_command("runserver", Server(host="localhost", port=5000, use_debugger=True, use_reloader=True))
-
+manager.add_command( "runserver",Server(host="localhost",port=app.config['SERVER_PORT'],use_debugger=True,use_reloader=True) )
 
 def main():
     manager.run()
-
 
 if __name__ == "__main__":
     try:
@@ -17,9 +15,3 @@ if __name__ == "__main__":
     except Exception as e:
         import traceback
         traceback.print_exc()
-
-
-
-
-
-
